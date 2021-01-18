dd7deb6f7b4ea8f6d347dbc88ce27870c23d175f
cheersmas <shradhankr0028@gmail.com>
2021-01-18 10:42:22 +0530

    Updates

---
+++ precache-manifest.0e2986e60210d78542012ad771198025.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea87f4c32068722a5d6b6b4aec025b45",
+    "url": "/bottom-app-bar/index.html"
+  },
+  {
+    "revision": "7b65f0aec809aa8798ff",
+    "url": "/bottom-app-bar/static/css/main.ce4512e8.chunk.css"
+  },
+  {
+    "revision": "9908c9a0921e509c422f",
+    "url": "/bottom-app-bar/static/js/2.ca07c358.chunk.js"
+  },
+  {
+    "revision": "7b65f0aec809aa8798ff",
+    "url": "/bottom-app-bar/static/js/main.2402df5e.chunk.js"
+  },
+  {
+    "revision": "8ec0e64341a556ad1d7e",
+    "url": "/bottom-app-bar/static/js/runtime~main.1ef12a19.js"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/bottom-app-bar/static/media/Github.064ace47.png"
+  },
+  {
+    "revision": "d61202186ea5c5df5af7a2d0c6dd6a7d",
+    "url": "/bottom-app-bar/static/media/events.d6120218.svg"
+  },
+  {
+    "revision": "4f030d2b16a4017db0da0d062cf591a4",
+    "url": "/bottom-app-bar/static/media/featured.4f030d2b.svg"
+  },
+  {
+    "revision": "6cf2d98eb63cfce9183212ef27574bb8",
+    "url": "/bottom-app-bar/static/media/search.6cf2d98e.svg"
+  },
+  {
+    "revision": "ebf0c5e95ae51f1e37d87328f7a19299",
+    "url": "/bottom-app-bar/static/media/slider.ebf0c5e9.svg"
+  }
+]);
